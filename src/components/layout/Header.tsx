039d034b478ecae9d3d4cc086acfd7278a8a037d
Mehmet Türkan <mhturknn@gmail.com>
2026-03-02 14:44:01 +0300

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -72,8 +72,8 @@ export default function Header() {
       {/* Mobile Nav */}
       <div
         className={clsx(
-          "border-t border-slate-200 bg-white px-4 py-3 md:hidden transition duration-300",
-          mobileOpen ? "h-auto opacity-100" : "h-0 opacity-0",
+          "border-t border-slate-200 bg-white px-4 md:hidden transition duration-300",
+          mobileOpen ? "h-auto opacity-100 py-3" : "h-0 opacity-0",
         )}
       >
         <nav className="flex flex-col gap-1">
